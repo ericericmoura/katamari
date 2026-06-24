7696fc7148b97d327f412a01672d6213d54999b8
Wr3tchedTorch <ericmoura511@gmail.com>
2026-06-24 12:00:04 -0300

    made it possible to relate tags with ids; and retrieve and entity using their ID

--- EntityManager.cpp
+++ EntityManager.cpp
@@ -1,9 +1,12 @@
 #include "EntityManager.h"
 
 #include <utility>
+#include <string>
+#include <vector>
 
 #include "Entity.h"
 #include "EntityStore.h"
+#include <unordered_map>
 
 Id EntityManager::AddEntity(Entity&& entity)
 {
@@ -34,4 +37,39 @@ void EntityManager::DisableEntity(Id id)
 		inactive_entities_.emplace(it->first, std::move(it->second));
 		active_entities_.erase(it);
 	}
+}
+
+std::vector<Id> EntityManager::GetIdsByTag(const std::string& tag)
+{
+	auto it = ids_.find(tag);
+	if (it != ids_.end())
+	{
+		return it->second;
+	}
+	return std::vector<Id>();
+}
+
+void EntityManager::AddTagForId(const std::string& tag, Id id)
+{
+	ids_[tag].push_back(id);
+}
+
+Entity* EntityManager::GetEntityById(Id id)
+{	
+	auto entity = GetEntityById(id, active_entities_);
+	if (entity == nullptr)
+	{
+		entity = GetEntityById(id, inactive_entities_);
+	}
+	return entity;
+}
+
+Entity* EntityManager::GetEntityById(Id id, std::unordered_map<Id, Entity>& map)
+{
+	auto it = map.find(id);
+	if (it == map.end())
+	{
+		return nullptr;
+	}
+	return &it->second;
 }
--- EntityManager.h
+++ EntityManager.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <string>
+#include <vector>
+#include <unordered_map>
+
 #include "EntityStore.h"
 #include "Entity.h"
 
@@ -11,7 +15,16 @@ public:
 	void EnableEntity(Id id);
 	void DisableEntity(Id id);
 
+	std::vector<Id> GetIdsByTag(const std::string& tag);
+	void AddTagForId(const std::string& tag, Id id);
+
+	Entity* GetEntityById(Id id);
+
 private:
 	Id current_id_ = 0;
+
+	std::unordered_map<std::string, std::vector<Id>> ids_;
+
+	Entity* GetEntityById(Id id, std::unordered_map<Id, Entity>& map);
 };
 
